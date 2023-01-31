76873013fa7cc675684867baab3da711a4e72e87
Lili <lidiaechuacas@hotmail.com>
2023-01-31 19:46:26 +0100

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MichiRefugio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
