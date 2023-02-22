e33b414452cdcbd03391701d192353fccf1e55e4
Lili <lidiaechuacas@hotmail.com>
2023-02-22 19:38:12 +0100

    sansonchan
    empezan2 pagina de sanson :sunny:

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,14 +6,13 @@ namespace MichiRefugio.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PaginaSanson()
         {
             return View();
         }
