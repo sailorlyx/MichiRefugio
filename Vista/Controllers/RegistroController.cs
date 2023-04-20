992ec70393450d512a5f8308b8bb79782378541d
Lili <lidiaechuacas@hotmail.com>
2023-04-20 14:27:08 +0200

    lidia 1 modal 32442 :+1:
    la clave es no rendirse nunca

--- Vista/Controllers/RegistroController.cs
+++ Vista/Controllers/RegistroController.cs
@@ -10,32 +10,32 @@ namespace Vista.Controllers
             return View();
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult CrearUsuario(UsuarioModel usuario)
-        {
-            if (ModelState.IsValid)
-            {
-                var nuevoUsuario = new UsuarioModel //preguntar :(((
-            {
-                nombre = usuario.nombre,
-                apellido = usuario.apellido,
-                apellido2 = usuario.apellido2,
-                email = usuario.email,
-                contraseña = usuario.contraseña,
-                telefono = usuario.telefono,
-                //admin = usuario.admin .... esto no hace falta en verda??
-            };
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult CrearUsuario(UsuarioModel usuario)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        var nuevoUsuario = new UsuarioModel //preguntar :(((
+        //    {
+        //        nombre = usuario.nombre,
+        //        apellido = usuario.apellido,
+        //        apellido2 = usuario.apellido2,
+        //        email = usuario.email,
+        //        contraseña = usuario.contraseña,
+        //        telefono = usuario.telefono,
+        //        //admin = usuario.admin .... esto no hace falta en verda??
+        //    };
 
-                //Usuario.Add(usuario); ...........asi igual no es :p
-                //guardar cambios (mirar internet o preguntar)
+        //        //Usuario.Add(usuario); ...........asi igual no es :p
+        //        //guardar cambios (mirar internet o preguntar)
 
-                return RedirectToAction("Index", "Home");
+        //        return RedirectToAction("Index", "Home");
 
-            }
+        //    }
 
-            return View(usuario);
-        }
+        //    return View(usuario);
+        //}
 
 
     }
--- Vista/wwwroot/js/site.js
+++ Vista/wwwroot/js/site.js
@@ -15,3 +15,21 @@ window.addEventListener("click", function (event) {
         modal.style.display = "none";
     }
 });
+
+var segundoModal = document.getElementById('segundoModal');
+var botonModal = document.getElementById('abrirSegundoModal');
+var cerrarModal = document.getElementsByClassName("cerrarModal")[1];
+
+botonModal.onclick = function () {
+    segundoModal.style.display = "block";
+}
+
+cerrarModal.onclick = function () {
+    segundoModal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == segundoModal) {
+        segundoModal.style.display = "none";
+    }
+}
